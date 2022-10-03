220c94f15d336cd825080f50b8a4fd9901f16058
Abdullahi Bashiir <Abdullahid.bashiir@gmail.com>
2022-10-03 05:57:29 -0700

    New Project

--- Day 1 Projact 1 Expanding Cards/app.js
+++ Day 1 Projact 1 Expanding Cards/app.js
@@ -1,14 +1,14 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll(".panel");
 // added new comment
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
 
 function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
 }
